157a68083a8bb9c72fe2474d7ea7386be598324b
suthaan <suthaan23@gmail.com>
2024-12-06 21:40:06 +0530

    saved

--- grocerystore/settings.py
+++ grocerystore/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/images'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Add your custom static folder here
